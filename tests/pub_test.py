56e3e17a3f2b8009030de782fd693dd8afe62b33
Clau <clau.rjimenez@gmail.com>
2021-11-24 11:06:06 +0000

    Added tests for Pub

--- tests/pub_test.py
+++ tests/pub_test.py
@@ -3,4 +3,14 @@ from src.pub import Pub
 
 
 class TestPub(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.pub = Pub("El Tigre", 1000.0, [])
+
+    def test_pub_name(self):
+        self.assertEqual("El Tigre", self.pub.name)
+
+    def test_pub_price(self):
+        self.assertEqual(1000.0, self.pub.price)
+
+    def test_pub_drinks_list(self):
+        self.assertEqual([], self.pub.drinks_list)
