f39839716d6d2bf07bdf7b20907e1cb4a299d3f2
Clau <clau.rjimenez@gmail.com>
2021-11-24 11:11:04 +0000

    Added tests for Customer class

--- tests/customer_test.py
+++ tests/customer_test.py
@@ -3,4 +3,14 @@ from src.customer import Customer
 
 
 class TestCustomer(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.customer = Customer("Pedro Sanchez", 10, 50.0)
+
+    def test_customer_name(self):
+        self.assertEqual("Pedro Sanchez", self.customer.name)
+
+    def test_customer_stomach(self):
+        self.assertEqual(10, self.customer.stomach)
+
+    def test_customer_wallet(self):
+        self.assertEqual(50.0, self.customer.wallet)
